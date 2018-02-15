8dc4919f9be295d2a415bbb198f7e51dde1f1f35
Azizou Ogbone <ogboneazizou@gmail.com>
2018-02-15 19:52:14 +0200

    pouch db tutorial follow through

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NewPersonPage } from '../new-person/new-person';
+import PouchDB from 'pouchdb';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private persons;
+  private db;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidEnter(){
+   this.refresh()
+  }
+
+  refresh() {
+    this.db = new PouchDB('contacts');
+    this.persons = [];
+
+    this.db.allDocs({include_docs: true}, (err, result) => {
+      if(!err){
+        let rows = result.rows;
+        for(let i=0; i < rows.length; i++){
+          this.persons.push(rows[i].doc);
+        }
+        console.log(rows)
+      }
+    })
+  }
+
+  createNew(){
+    this.navCtrl.push(NewPersonPage)
+    
   }
 
 }
